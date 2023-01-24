017fc92a557561ee44ff92bd3de01e2319d672be
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-01-24 16:55:25 -0500

    Create PersonalInfo.cs

---
+++ PersonalInfo.cs
@@ -0,0 +1,24 @@
+/* 
+ * Description: 
+ * Write, compile, and test a program named PersonalInfo that displays a person’s name, birthdate, work phone number, and cell phone number on separate lines.
+ * Follow the format in the example below:
+* Lucinda Potter
+* 6/24/1992
+* work 000-555-0101
+* cell 000-555-0189
+* The user enters weight, feet, and inches instead of just inches and then computes BMI 
+*/
+
+using System;
+using static System.Console;
+class PersonalInfo
+{
+   static void Main()
+   {
+      // Write your main here
+      WriteLine("Lucinda Potter");
+      WriteLine("6/24/1992");
+      WriteLine("work 000-555-0101");
+      WriteLine("cell 000-555-0189");
+   }
+}
