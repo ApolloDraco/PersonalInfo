38e4e9cf7becc43d1ee6e1f0f0f91fb9ea52198c
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-01-24 16:56:33 -0500

    Update PersonalInfo.cs

--- PersonalInfo.cs
+++ PersonalInfo.cs
@@ -2,11 +2,10 @@
  * Description: 
  * Write, compile, and test a program named PersonalInfo that displays a person’s name, birthdate, work phone number, and cell phone number on separate lines.
  * Follow the format in the example below:
-* Lucinda Potter
-* 6/24/1992
-* work 000-555-0101
-* cell 000-555-0189
-* The user enters weight, feet, and inches instead of just inches and then computes BMI 
+ * Lucinda Potter
+ * 6/24/1992
+ * work 000-555-0101
+ * cell 000-555-0189
 */
 
 using System;
